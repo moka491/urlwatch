822bd6a0118b26fe60c061c29f6e4c7227d4bb5b
Jannis Becker <jbecker@jannisbecker.me>
2020-08-24 22:33:21 +0200

    Update start.sh with echo prints

--- docker/start.sh
+++ docker/start.sh
@@ -3,8 +3,12 @@
 # Grab custom interval from env variable or use default (every minute)
 CRON_RULE=${INTERVAL_CRON:-"* * * * *"}
 
-# Remove old cronjobs and add new one
+# Remove old cronjobs
+echo "Cleaning up old cronjobs"
 crontab -r 2&>1 /dev/null
+
+# And add new one with the correct interval
+echo "Adding new urlwatch cronjob: $CRON_RULE"
 echo "$CRON_RULE cd /root/.urlwatch && /opt/bitnami/python/bin/urlwatch --urls config/urls.yaml --config config/urlwatch.yaml --hooks config/hooks.py --cache cache.db > /proc/1/fd/1 2>/proc/1/fd/2" | crontab -
 
 # Run cron
